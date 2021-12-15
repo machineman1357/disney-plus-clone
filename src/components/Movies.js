42766daeab8a218c0d9190155d7094a587059691
machineman1357 <dgeichert@gmail.com>
2021-12-14 17:57:43 -0800

    Movies component

---
+++ src/components/Movies.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components";
+
+export default function Movies() {
+    return (
+        <Container>
+            Movies
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    position: relative;
+`;
